28c2f3349072cbc0b851c48a9f137c5c0d95e699
LowUp <credo05@hotmail.fr>
2022-02-09 13:12:48 +0100

    upgrade

--- Madlib.py
+++ Madlib.py
@@ -10,20 +10,20 @@ words = madlibs_class(missing_word)
 
 def Text():
     text = [
-        ["It was " + words.get_missing_word(0) + ", cold November day. "],
-        ["I woke up to the " + words.get_missing_word(1)],
-        ["smell of " + words.get_missing_word(2)],
-        ["roasting in the " + words.get_missing_word(3) + " dowstairs."],
-        ["I " + words.get_missing_word(4) + " down the stairs to see if I could help"],
-        [words.get_missing_word(5) + " the dinner."],
-        ["My mom said, 'See if '" + words.get_missing_word(6)],
-        ["needs a fresh" + words.get_missing_word(7)],
-        ["So I carried a tray of glasses full of " + words.get_missing_word(8)],
-        ["into the " + words.get_missing_word(9) + " room."],
-        ["When I got there, I couldn't believe my " + words.get_missing_word(10) + " !"],
-        ["There were " + words.get_missing_word(11)],
-        [words.get_missing_word(12) + " on the"],
-        [words.get_missing_word(13) + " !"]
+        [f"It was {words.get_missing_word(0)}, cold November day. "],
+        [f"I woke up to the {words.get_missing_word(1)}"],
+        [f"smell of {words.get_missing_word(2)}"],
+        [f"roasting in the {words.get_missing_word(3)} dowstairs."],
+        [f"I {words.get_missing_word(4)} down the stairs to see if I could help"],
+        [f"{words.get_missing_word(5)} the dinner."],
+        [f"My mom said, 'See if ' {words.get_missing_word(6)}"],
+        [f"needs a fresh {words.get_missing_word(7)}"],
+        [f"So I carried a tray of glasses full of {words.get_missing_word(8)}"],
+        [f"into the {words.get_missing_word(9)} room."],
+        [f"When I got there, I couldn't believe my {words.get_missing_word(10)} !"],
+        [f"There were {words.get_missing_word(11)}"],
+        [f"{words.get_missing_word(12)} on the"],
+        [f"{words.get_missing_word(13)} !"]
     ]
     return text
 
